03c390c50a650a17aa59c30e8879f25d2904979c
MK-404 <78595019+MK-404@users.noreply.github.com>
2023-11-29 17:08:34 +0100

    1.0

--- config/config-example.js
+++ config/config-example.js
@@ -1,34 +1,45 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0";
+Config.version = "0";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
 Config.whitelist = [
 	'wikipedia.org',
-
-	// The full list is maintained outside of this repository so changes to it
-	// don't clutter the commit log. Feel free to copy our list for your own
-	// purposes; it's here: https://play.pokemonshowdown.com/config/config.js
-
-	// If you would like to change our list, simply message Zarel on Smogon or
-	// Discord.
 ];
 
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
-	port: 443,
-	httpport: 8000,
+	host: 'budewinn.it',
+	port: 2053,
+	httpport: 2095,
 	altport: 80,
-	registered: true
+	registered: true,
+	https: true,
 };
 
 Config.roomsFirstOpenScript = function () {
 };
 
 Config.customcolors = {
-	'zarel': 'aeo'
-};
+	
+	"mkaya": "pqhfa97b",
+	"pala97": "vzir7urz",
+	
+	"peonygymleader": "s4nmqutt",
+	"kabucapopalestra": "k1j0j3mp",
+	"capopalestrasofora": "z4bgmnho",
+	"capopalestramary": "65wc9zlb",
+	"capopalestrabeet": "yiib1bl0",
+	"capopalestrarayne": "yiib1bl0",
+	"capopalestrasavory": "e5zzduh9",
+	"capopalestrayarrow": "mu9hs543",
+	"oniongymleader": "6ukd8gvk",
+	"laburnogymleader": "pqhfa97b",
+	"azzurragymleader": "zlhuz0gj",
+	"capopalestrafabia": "a56nunyg",
+	"gymleadermelania": "sxnhbl2e",
+	"capopalestramilo": "4j37cqhj",	
+};;
