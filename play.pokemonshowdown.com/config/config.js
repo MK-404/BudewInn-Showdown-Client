d6b731d18100e8adf893bbbee2ac3a89beba3bdb
MK-404 <78595019+MK-404@users.noreply.github.com>
2024-01-26 07:55:20 +0100

    config fix

--- play.pokemonshowdown.com/config/config.js
+++ play.pokemonshowdown.com/config/config.js
@@ -1,1 +1,57 @@
-"../../config/config.js"
+var Config = Config || {};
+
+Config.version = "0";
+
+Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
+
+Config.whitelist = [
+	'wikipedia.org',
+];
+
+// `defaultserver` specifies the server to use when the domain name in the
+// address bar is `Config.routes.client`.
+Config.defaultserver = {
+	id: 'showdown',
+	host: 'budewinn.it',
+	port: 2053,
+	httpport: 2095,
+	altport: 80,
+	registered: true,
+	https: true,
+	//TODO: lo scrivo qui ma è per la macchina che fa girare tutto: quando pusho su github il client (prima devo pushare il server), fa fetchare su github prima gli sprite poi il client, e alla fine poi builda da solo 
+};
+
+Config.roomsFirstOpenScript = function () {
+};
+
+Config.customcolors = {
+	
+	"mkaya": "pqhfa97b",
+	"pala97": "vzir7urz",
+	
+	"peonygymleader": "s4nmqutt",
+	"kabucapopalestra": "k1j0j3mp",
+	"capopalestrasofora": "z4bgmnho",
+	"capopalestramary": "65wc9zlb",
+	"capopalestrabeet": "yiib1bl0",
+	"capopalestrarayne": "yiib1bl0",
+	"capopalestrasavory": "e5zzduh9",
+	"capopalestrayarrow": "mu9hs543",
+	"oniongymleader": "6ukd8gvk",
+	"laburnogymleader": "pqhfa97b",
+	"azzurragymleader": "zlhuz0gj",
+	"capopalestrafabia": "a56nunyg",
+	"gymleadermelania": "sxnhbl2e",
+	"capopalestramilo": "4j37cqhj",	
+};;
+/*** Begin automatically generated configuration ***/
+Config.version = "0.11.2 (c50503c3)";
+
+Config.routes = {
+	root: 'pokemonshowdown.com',
+	client: 'budewinn.it',
+	dex: 'dex.pokemonshowdown.com',
+	replays: 'replay.pokemonshowdown.com',
+	users: 'pokemonshowdown.com/users',
+};
+/*** End automatically generated configuration ***/
